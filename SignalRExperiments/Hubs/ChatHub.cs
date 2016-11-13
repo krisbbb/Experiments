ba877af2330383c2fdb60a7a8b72961acac92fa3
krisbbb <krisbbb@gmail.com>
2016-11-12 21:37:52 -0800

    Starting web app

---
+++ SignalRExperiments/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+
+public class ChatHub : Hub
+{
+    public static List<string> ConnectedUsers;
+
+    public void Send(string originatorUser, string message)
+    {
+        Clients.All.messageReceived(originatorUser, message);
+    }
+
+    public void Connect(string newUser)
+    {
+        if (ConnectedUsers == null)
+            ConnectedUsers = new List<string>();
+
+        ConnectedUsers.Add(newUser);
+        Clients.Caller.getConnectedUsers(ConnectedUsers);
+        Clients.Others.newUserAdded(newUser);
+    }
+}
